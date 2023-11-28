c19ccbd616f1ce366f97bd72ae687e9c297e8bcb
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2023-11-28 15:27:28 +0100

    Setting control prop from input

--- src/client/Pg.LetsExercise.CircleProgressControl/CircleProgressControl/CircleProgress.tsx
+++ src/client/Pg.LetsExercise.CircleProgressControl/CircleProgressControl/CircleProgress.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { Label } from '@fluentui/react';
 
 export interface ICircleProgressProps {
-  name?: string;
+  percentage?: number;
 }
 
 export class CircleProgress extends React.Component<ICircleProgressProps> {
   public render(): React.ReactNode {
     return (
       <Label>
-        {this.props.name}
+        {this.props.percentage}
       </Label>
     )
   }
