47663c18a0b9262d9760025ebb093dd9758580ca
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2023-12-15 07:58:39 +0100

    Unit test template project added

---
+++ src/server/Pg.LetsExercise/Pg.LetsExercise.Plugins.Tests/DemoTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Pg.LetsExercise.Plugins.Tests
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true); 
+        }   
+    }
+}
