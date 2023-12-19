ebcadc4dfe40d0f37d11d85a5dc96cc2962ae4ac
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2023-12-19 13:21:37 +0100

    DataRepository initial implementation

--- src/server/Pg.LetsExercise/Plugins/Infrastructure/DataRepository.cs
+++ src/server/Pg.LetsExercise/Plugins/Infrastructure/DataRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.Xrm.Sdk;
 using Pg.LetsExercise.Plugins.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Pg.LetsExercise.Plugins.Infrastructure
 {
@@ -17,21 +18,61 @@ namespace Pg.LetsExercise.Plugins.Infrastructure
 
         public IList<pg_exerciserecord> GetCurrentDayRecords(DateTime now, Guid ownerId, pg_exerciseset exercise)
         {
-            throw new NotImplementedException();
+            using (var context = new DataverseContext(_service))
+            {
+                var query = context.pg_exerciserecordSet
+                    .Where(e => e.OwnerId.Id == ownerId && e.pg_date.Value == now)
+                    .Select(ep => ep);
+
+                return query.ToList<pg_exerciserecord>();
+            }
         }
         public IList<pg_exerciserecord> GetCurrentWeekRecords(DateTime now, Guid ownerId, pg_exerciseset exercise)
         {
-            throw new NotImplementedException();
+            var startOfWeek = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek);
+            var endOfWeek = startOfWeek.AddDays(7);
+
+            using (var context = new DataverseContext(_service))
+            {
+                var query = context.pg_exerciserecordSet
+                    .Where(e => e.OwnerId.Id == ownerId && e.pg_date.Value >= startOfWeek
+                        && e.pg_date.Value <= endOfWeek)
+                    .Select(ep => ep);
+
+                return query.ToList<pg_exerciserecord>();
+            }
         }
 
         public IList<pg_exerciserecord> GetCurrentMonthRecords(DateTime now, Guid ownerId, pg_exerciseset exercise)
         {
-            throw new NotImplementedException();
+            var firstDayOfMonth = new DateTime(now.Year, now.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+
+            using (var context = new DataverseContext(_service))
+            {
+                var query = context.pg_exerciserecordSet
+                    .Where(e => e.OwnerId.Id == ownerId && e.pg_date.Value >= firstDayOfMonth
+                        && e.pg_date.Value <= lastDayOfMonth)
+                    .Select(ep => ep);
+
+                return query.ToList<pg_exerciserecord>();
+            }
         }
 
         public IList<pg_exerciserecord> GetCurrentYearRecords(DateTime now, Guid ownerId, pg_exerciseset exercise)
         {
-            throw new NotImplementedException();
+            DateTime firstDayOfYear = new DateTime(now.Year, 1, 1);
+            DateTime lastDayOfYear = new DateTime(now.Year, 12, 31);
+
+            using (var context = new DataverseContext(_service))
+            {
+                var query = context.pg_exerciserecordSet
+                    .Where(e => e.OwnerId.Id == ownerId && e.pg_date.Value >= firstDayOfYear
+                        && e.pg_date.Value <= lastDayOfYear)
+                    .Select(ep => ep);
+
+                return query.ToList<pg_exerciserecord>();
+            }
         }
 
         public pg_exercisegoal GetGoal(Guid goalId)
