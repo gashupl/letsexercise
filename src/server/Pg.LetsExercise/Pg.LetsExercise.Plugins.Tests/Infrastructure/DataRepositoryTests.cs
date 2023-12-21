6bdef84c03aec7f1be3c2a22dc119a5c382697b4
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2023-12-21 08:51:41 +0100

    Empty unit tests templates added

---
+++ src/server/Pg.LetsExercise/Pg.LetsExercise.Plugins.Tests/Infrastructure/DataRepositoryTests.cs
@@ -0,0 +1,50 @@
+﻿using Pg.LetsExercise.Plugins.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pg.LetsExercise.Plugins.Tests.Infrastructure
+{
+    public class DataRepositoryTests
+    {
+        [Fact]
+        public void GetCurrentDayRecords_ReturnsList()
+        {
+            throw new NotImplementedException(); 
+        }
+
+        [Fact]
+        public void GetCurrentWeekRecords_ReturnsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetCurrentMonthRecords_ReturnsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetCurrentYearRecords_ReturnsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetGoal_ReturnGoal()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetGoal_MissingGoal_ThrowsException()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
