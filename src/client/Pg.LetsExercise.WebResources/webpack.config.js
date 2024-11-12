dc6bdb889100bdfffcb8088bc3e2de9690153d87
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2024-11-12 08:39:25 +0100

    Add Goals Monthly Chart entry to webpack configuration for dynamic output

--- src/client/Pg.LetsExercise.WebResources/webpack.config.js
+++ src/client/Pg.LetsExercise.WebResources/webpack.config.js
@@ -3,11 +3,12 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
     devtool: "source-map",
     entry: {
-        Main: "./src/Main"
+        Main: "./src/Main", 
+        GoalsMonthlyChart: "./src/pages/goalsmonthlychart.ts"
     },
     output: {
-        filename: "letsexercise-main.js",
-        sourceMapFilename: "maps/letsexercise-main.js.map",
+        filename: "[name].js", // Use [name] to dynamically name the output files
+        sourceMapFilename: "maps/[name].js.map", // Use [name] for source maps
         path: path.resolve(__dirname, "./Webresources/scripts"),
         library: ["LetsExercise"],
         libraryTarget: "var",
