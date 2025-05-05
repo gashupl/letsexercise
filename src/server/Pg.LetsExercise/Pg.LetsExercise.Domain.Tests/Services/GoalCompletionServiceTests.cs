58186186818c9cb71aa548f24515371326a07ea1
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-05-05 08:24:53 +0200

    Refactoring testing code

---
+++ src/server/Pg.LetsExercise/Pg.LetsExercise.Domain.Tests/Services/GoalCompletionServiceTests.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.Xrm.Sdk;
+using Moq;
+using Pg.LetsExercis.Domain.Services;
+using Pg.LetsExercise.Domain.Repositories;
+using Pg.LetsExercise.Model;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Pg.LetsExercise.Domain.Tests.Services
+{
+    public class GoalCompletionServiceTests
+    {
+        private readonly Mock<ITracingService> _tracingServiceMock; 
+
+        public GoalCompletionServiceTests()
+        {
+            _tracingServiceMock = new Mock<ITracingService>();
+            _tracingServiceMock.Setup(t => t.Trace(It.IsAny<string>()));
+        }   
+        [Fact]
+        public void GetPercentage_ExpectedScoreIsZero_ThrowsArgumentException()
+        {
+            // Arrange
+            var service = new GoalCompletionService(null, null);
+            var records = new List<pg_exerciserecord>();
+            var expectedScore = 0;
+
+            // Act
+            var exception = Assert.Throws<ArgumentException>(() => service.GetPercentage(records, expectedScore));
+
+            // Assert
+            Assert.Equal("Expected score must be greater than 0", exception.Message);
+        }
+
+        [Fact]
+        public void GetPercentage_EmptyList_ReturnsZero()
+        {
+            // Arrange
+            var service = new GoalCompletionService(null, _tracingServiceMock.Object);
+            var records = new List<pg_exerciserecord>();
+            var expectedScore = 100;
+            var expected = 0; // 0%
+
+            // Act
+            var actual = service.GetPercentage(records, expectedScore);
+
+            // Assert
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void GetPercentage_ValidList_Returns25()
+        {
+            // Arrange
+            var service = new GoalCompletionService(null, _tracingServiceMock.Object);
+            var records = new List<pg_exerciserecord>() {
+                new pg_exerciserecord { pg_scorenumber = 20 },
+                new pg_exerciserecord { pg_scorenumber = 5 }
+            };
+            var expectedScore = 100;
+            var expected = 25; // 25%
+
+            // Act
+            var actual = service.GetPercentage(records, expectedScore);
+
+            // Assert
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void GetPercentage_ValidList_Returns24()
+        {
+            // Arrange
+            var service = new GoalCompletionService(null, _tracingServiceMock.Object);
+            var records = new List<pg_exerciserecord>() {
+                new pg_exerciserecord { pg_scorenumber = 20 },
+                new pg_exerciserecord { pg_scorenumber = 5 }
+            };
+            var expectedScore = 101;
+            var expected = 24; // 24%
+
+            // Act
+            var actual = service.GetPercentage(records, expectedScore);
+
+            // Assert
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void GetCompletionPercentage_DailyGoal_ReturnPercentage()
+        {
+            //Arrange
+            var expected = 50; // 50%
+
+            //Act
+            var actual = GetActualPercentage(pg_exercisegoaltypeset.Daily); 
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetCompletionPercentage_WeeklyGoal_ReturnPercentage()
+        {
+            //Arrange
+            var expected = 50; // 50%
+
+            //Act
+            var actual = GetActualPercentage(pg_exercisegoaltypeset.Weekly);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void GetCompletionPercentage_MonthlyGoal_ReturnPercentage()
+        {
+            //Arrange
+            var expected = 50; // 50%
+
+            //Act
+            var actual = GetActualPercentage(pg_exercisegoaltypeset.Monthly);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void GetCompletionPercentage_AnnualGoal_ReturnPercentage()
+        {
+            //Arrange
+            var expected = 50; // 50%
+
+            //Act
+            var actual = GetActualPercentage(pg_exercisegoaltypeset.Yearly);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetCompletionPercentage_MissingGoalParameters_ThrowsException()
+        {
+            //Arrange
+            var repo = new Mock<IRepository>();
+            repo.Setup(r => r.GetGoal(It.IsAny<Guid>())).Returns(new pg_exercisegoal());
+            var service = new GoalCompletionService(repo.Object, _tracingServiceMock.Object);
+
+            //Act
+            var exception = Assert.Throws<InvalidPluginExecutionException>(() => service.GetCompletionPercentage(Guid.NewGuid()));
+
+            //Assert
+            Assert.Equal("Not enough data to calculate goal completion", exception.Message);
+        }
+
+        private int GetActualPercentage(pg_exercisegoaltypeset type)
+        {
+            var repo = GetMockedRepository(type);
+            var service = new GoalCompletionService(repo, _tracingServiceMock.Object);
+            return service.GetCompletionPercentage(Guid.NewGuid());
+        }
+
+        private IRepository GetMockedRepository(pg_exercisegoaltypeset type)
+        {
+
+            var list = new List<pg_exerciserecord> { new pg_exerciserecord { pg_scorenumber = 5 } }; 
+
+            var repo = new Mock<IRepository>();
+            repo.Setup(r => r.GetGoal(It.IsAny<Guid>())).Returns(new pg_exercisegoal
+            {
+                pg_goaltype = type,
+                pg_scorenumber = 10, 
+                pg_Exercise = pg_exerciseset.Pushups, 
+                OwnerId = new EntityReference { Id = Guid.NewGuid() }   
+            });
+
+            repo.Setup(r => r.GetCurrentDayRecords(
+                It.IsAny<DateTime>(), It.IsAny<Guid>(), It.IsAny<pg_exerciseset>()))
+                .Returns(list);
+
+            repo.Setup(r => r.GetCurrentWeekRecords(
+                It.IsAny<DateTime>(), It.IsAny<Guid>(), It.IsAny<pg_exerciseset>()))
+                .Returns(list);
+
+            repo.Setup(r => r.GetCurrentMonthRecords(
+                It.IsAny<DateTime>(), It.IsAny<Guid>(), It.IsAny<pg_exerciseset>()))
+                .Returns(list);
+
+            repo.Setup(r => r.GetCurrentYearRecords(
+                It.IsAny<DateTime>(), It.IsAny<Guid>(), It.IsAny<pg_exerciseset>()))
+                .Returns(list);
+
+            return repo.Object; 
+        }
+    }
+}
---
+++ src/server/Pg.LetsExercise/Pg.LetsExercise.Infrastructure.Tests/Repositories/DataRepositoryTests.cs
@@ -0,0 +1,130 @@
+﻿using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using Pg.LetsExercise.Infrastructure.Repositories;
+using Pg.LetsExercise.Model;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace Pg.LetsExercise.Plugins.Tests.Infrastructure
+{
+    public class DataRepositoryTests
+    {
+        private readonly IOrganizationService _service;
+        private readonly Guid _ownerId = Guid.NewGuid(); 
+        private readonly Guid _goalId = Guid.NewGuid();
+
+        public DataRepositoryTests()
+        {
+            _service = InitDataService();
+        }
+        
+        [Fact]
+        public void GetCurrentDayRecords_ReturnsSingleRecord()
+        {
+            var repo = new DataRepository(_service); 
+            var actual = repo.GetCurrentDayRecords(new DateTime(2024, 01, 01, 11, 22, 33), _ownerId, pg_exerciseset.Pushups);
+            Assert.Single(actual); 
+        }
+
+        [Fact]
+        public void GetCurrentDayRecords_ReturnsEmptyList()
+        {
+            var repo = new DataRepository(_service);
+            var actual = repo.GetCurrentDayRecords(new DateTime(2023, 01, 01), _ownerId, pg_exerciseset.Pushups);
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCurrentWeekRecords_ReturnsSingleRecord()
+        {
+            var repo = new DataRepository(_service);
+            var actual = repo.GetCurrentWeekRecords(new DateTime(2024, 01, 02), _ownerId, pg_exerciseset.Pushups);
+            Assert.Single(actual);
+        }
+
+        [Fact]
+        public void GetCurrentWeekRecords_ReturnsEmptyList()
+        {
+            var repo = new DataRepository(_service);
+            var actual = repo.GetCurrentWeekRecords(new DateTime(2024, 01, 8), _ownerId, pg_exerciseset.Pushups);
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCurrentMonthRecords_ReturnsSingleRecord()
+        {
+            var repo = new DataRepository(_service);
+            var actual = repo.GetCurrentMonthRecords(new DateTime(2024, 01, 8), _ownerId, pg_exerciseset.Pushups);
+            Assert.Single(actual);
+        }
+
+        [Fact]
+        public void GetCurrentMonthRecords_ReturnsEmptyList()
+        {
+            var repo = new DataRepository(_service);
+            var actual = repo.GetCurrentMonthRecords(new DateTime(2024, 02, 01), _ownerId, pg_exerciseset.Pushups);
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCurrentYearRecords_ReturnsSingleRecord()
+        {
+            var repo = new DataRepository(_service);
+            var actual = repo.GetCurrentYearRecords(new DateTime(2024, 06, 8), _ownerId, pg_exerciseset.Pushups);
+            Assert.Single(actual);
+        }
+
+        [Fact]
+        public void GetCurrentYearRecords_ReturnsEmptyList()
+        {
+            var repo = new DataRepository(_service);
+            var actual = repo.GetCurrentYearRecords(new DateTime(2025, 01, 01), _ownerId, pg_exerciseset.Pushups);
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetGoal_ReturnGoal()
+        {
+            var repo = new DataRepository(_service);
+            var actual = repo.GetGoal(_goalId); 
+            Assert.Equal(_goalId, actual.Id);
+        }
+
+        [Fact]
+        public void GetGoal_MissingGoal_ThrowsException()
+        {
+            var repo = new DataRepository(_service);
+            Assert.Throws<InvalidPluginExecutionException>(() => repo.GetGoal(Guid.NewGuid()));
+        }
+
+        public IOrganizationService InitDataService()
+        {
+            var context = new XrmFakedContext(); 
+            context.ProxyTypesAssembly = Assembly.GetAssembly(typeof(pg_exerciserecord));
+    
+            var record = new pg_exerciserecord
+            {
+                Id = Guid.NewGuid(),
+                pg_date = new DateTime(2024, 1, 1),
+                pg_exercise = pg_exerciseset.Pushups,
+                OwnerId = new EntityReference(SystemUser.EntityLogicalName, _ownerId)
+            }; 
+
+            var goal = new pg_exercisegoal
+            {
+                Id = _goalId,
+                pg_Exercise = pg_exerciseset.Pushups,
+                pg_goaltype = pg_exercisegoaltypeset.Daily,
+                pg_scorenumber = 10,
+                OwnerId = new EntityReference(SystemUser.EntityLogicalName, _ownerId)
+            };
+
+            context.Initialize(new List<Entity>() { record, goal });
+            return context.GetFakedOrganizationService();
+
+        }
+
+    }
+}
