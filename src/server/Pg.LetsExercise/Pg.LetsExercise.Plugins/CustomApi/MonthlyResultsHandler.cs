cb0228ab318a223b9ac587985586b6e10027fe79
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-05-20 08:34:20 +0200

    Solution export + new plugin template (MonthlyResults)

---
+++ src/server/Pg.LetsExercise/Pg.LetsExercise.Plugins/CustomApi/MonthlyResultsHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pg.LetsExercise.Plugins.CustomApi
+{
+    public class MonthlyResultsHandler : PluginBase
+    {
+        public MonthlyResultsHandler(Type pluginClassName) : base(pluginClassName)
+        {
+        }
+
+        protected override void ExecuteDataversePlugin(ILocalPluginContext localPluginContext)
+        {
+
+        }
+    }
+}
