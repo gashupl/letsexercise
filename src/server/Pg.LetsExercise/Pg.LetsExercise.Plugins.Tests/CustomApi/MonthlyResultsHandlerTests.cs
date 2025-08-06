b452872a131c104d941447fecb4ae4a9a8c9ea7d
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-08-06 09:13:53 +0200

    Add MonthlyResultsHandlerTests with Moq dependencies

---
+++ src/server/Pg.LetsExercise/Pg.LetsExercise.Plugins.Tests/CustomApi/MonthlyResultsHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using Pg.LetsExercise.Domain.Services;
+using Pg.LetsExercise.Plugins.CustomApi;
+using Xunit;
+
+namespace Pg.LetsExercise.Plugins.Tests.CustomApi
+{
+    public class MonthlyResultsHandlerTests
+    {
+        [Fact]
+        public void CanExecute_EmptyLocalContext_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var inputToDateServiceMock = new Mock<IInputParameterToDateService>();
+            var parseToJsonServiceMock = new Mock<IParseToJsonService>();
+            var sumResultsServiceMock = new Mock<ISumResultsService>();
+
+            var handler = new MonthlyResultHandler
+                (inputToDateServiceMock.Object, parseToJsonServiceMock.Object, sumResultsServiceMock.Object);
+            // Act & Assert
+            Assert.True(handler.CanExecute()); 
+        }
+
+
+    }
+}
