e6f6841f5c4559f67bcb01973e37be01129215ea
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-08-06 09:01:58 +0200

    Remove unused using directives in tests

--- src/server/Pg.LetsExercise/Pg.LetsExercise.Domain.Tests/Services/SumResultsServiceTests.cs
+++ src/server/Pg.LetsExercise/Pg.LetsExercise.Domain.Tests/Services/SumResultsServiceTests.cs
@@ -5,9 +5,6 @@ using Pg.LetsExercise.Domain.Services;
 using Pg.LetsExercise.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Pg.LetsExercise.Domain.Tests.Services
