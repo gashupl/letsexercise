7bd099978f60c40ff32a648796c9efdd2571f817
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2026-03-16 08:29:20 +0100

    Add unified plugin tracing and logging abstraction

---
+++ src/server/Pg.LetsExercise/Pg.LetsExercise.Domain/Services/IPluginTracingService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xrm.Sdk.PluginTelemetry;
+
+namespace Pg.LetsExercise.Domain.Services
+{
+    public interface IPluginTracingService
+    {
+        void Trace(LogLevel logLevel, string format, params object[] args);
+    }
+}
